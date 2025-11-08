46985b35367d497bd6ea16b67a73c52d0db68768
Ramon Asuncion <asuncionbatista@gmail.com>
2025-11-08 00:51:07 -0500

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/user/bin/env bash
+# This is to create new models. I have my code all of the places. Just an easy way to do this for now
+rm -rf dataset
+rm -rf third_party/TextyMcSpeechy/tts_dojo/DATASETS/custom_voice
+rm -rf third_party/TextyMcSpeechy/tts_dojo/custom_voice_dojo
